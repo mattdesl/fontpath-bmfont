4eb5a7532a2f2533b31bd4c3df1fd3bc5c1f07d6
mattdesl <dave.des@gmail.com>
2014-10-16 17:03:46 -0400

    adding files

---
+++ index.js
@@ -0,0 +1,38 @@
+function glyph(data) {
+    return {
+        page: data.page,
+        hbx: data.xoffset,
+        hby: data.yoffset,
+        width: data.width,
+        height: data.height,
+        xoff: data.xadvance,
+        x: data.x,
+        y: data.y
+    }
+}
+
+function glyphs(chars) {
+    var out = {}
+    chars.forEach(function(c) {
+        out[String.fromCharCode(c.id)] = glyph(c)
+    })
+    return out
+}
+
+module.exports = function(bmfont) {
+    return {
+        pages: bmfont.pages,
+        resolution: 72,
+        size: bmfont.info.size,
+        units_per_EM: 1,
+        bitmap: true,
+        family_name: bmfont.info.face,
+        height: bmfont.common.lineHeight,
+        descender: bmfont.common.base,
+        ascender: bmfont.common.base,
+        glyphs: glyphs(bmfont.chars),
+        kerning: bmfont.kernings.map(function(k) {
+            return [String.fromCharCode(k.first),String.fromCharCode(k.second),k.amount]
+        })
+    }
+}
---
+++ test.js
@@ -0,0 +1,7 @@
+var fontpathBitmap = require('./')
+var test = require('tape').test
+
+test('converts BMFont to fontpath format', function(t) {
+    
+    t.end()
+})
